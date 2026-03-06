3ead45a7e4f33c62418b68bc30bdc679d0d24caa
Andrew MacDonald <ammacdonald3@gmail.com>
2026-03-06 09:06:45 -0500

    cleaned up repo of extraneous files

--- start_gunicorn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PYTHONPATH="${PYTHONPATH}:$(pwd)"
-
-if [ -f ".env" ]; then
-    export $(cat .env | grep -v '^#' | xargs)
-fi
-
-gunicorn -c gunicorn.conf.py "app:create_app()"
